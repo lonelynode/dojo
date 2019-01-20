f50f17029f807a37d3e18f84c80e216f8fee93f5
martin.zhou@oocl.com <martin.zhou@oocl.com>
2019-01-20 21:34:55 +0800

    # add code for forth test

--- src/main/java/interstingNumber/CarMileage.java
+++ src/main/java/interstingNumber/CarMileage.java
@@ -1,5 +1,8 @@
 package interstingNumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarMileage {
 
   public static long isInteresting(long number) {
@@ -9,7 +12,38 @@ public class CarMileage {
     else {
       if(number % 10 == 0)
         return 1;
-      return 1;
+      else if(digitSequentialAndInc(number))
+        return 1;
+      return 0;
     }
+  }
+
+  private static boolean digitSequentialAndInc(long number) {
+    Long[] array = numberToDigitArray(number);
+    boolean isDigitSequentialAndInc = true;
+    for(int i = array.length - 1; i > 0; i--) {
+      if(array[i - 1] - array[i] == 1) {
+        isDigitSequentialAndInc = true;
+      }
+      else {
+        isDigitSequentialAndInc = false;
+        break;
+      }
+    }
+    return isDigitSequentialAndInc;
+  }
+
+  private static Long[] numberToDigitArray(long number) {
+    List<Long> digitList = new ArrayList<Long>();
+    long digit = number % 10;
+    digitList.add(digit);
+    number /= 10;
+    while(number > 0) {
+      digit = number % 10;
+      digitList.add(digit);
+      number /= 10;
+    }
+    Long[] digitArray = digitList.toArray(new Long[0]);
+    return digitArray;
   }
 }
